685ccce7bc9f3d5e1e0d6608bd898d6bc200cfc5
Jonathan Bell <jonathan@hencove.com>
2025-08-11 16:08:08 -0400

    Update debounce timings & error conditions

--- src/scripts/components/_animatedLine.js
+++ src/scripts/components/_animatedLine.js
@@ -2,13 +2,15 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".hc-animated-line", {
-  value: 100,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#progressMarker",
-    scrub: 0.3,
-    start: "top 95%",
-    end: "bottom 20%",
-  },
-});
+if (document.querySelectorAll(".hc-animated-line")) {
+  gsap.to(".hc-animated-line", {
+    value: 100,
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#progressMarker",
+      scrub: 0.3,
+      start: "top 95%",
+      end: "bottom 20%",
+    },
+  });
+}
