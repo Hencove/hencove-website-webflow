2545de7695c22ae054c080097c809b27ea20f5ef
Jonathan Bell <jonathan@hencove.com>
2025-08-18 16:52:41 -0400

    Automatically add the video src, and use hevc for safari

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+// Global configuration
+export const R2_BASE_URL =
+  "https://pub-aa058d2f7a144fc9b37b2046ac64d64a.r2.dev";
+
 // Import all CSS
 import "./styles/main.css";
 import "./styles/components/hero-hencurve.scss";
